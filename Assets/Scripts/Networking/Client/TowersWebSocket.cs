45f859cc34b7e3ad1adf8e1615fb372668d7ef84
heoliajdr <admin@heolia.eu>
2020-03-04 15:21:22 +0100

    Client adding websocket base with DLL on Assets/External

---
+++ Assets/Scripts/Networking/Client/TowersWebSocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using WebSocketSharp;
+
+namespace Networking.Client
+{
+    public class TowersWebSocket : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private WebSocket ws = new WebSocket ("wss://towers.heolia.eu/websocket");
+        public string call = "{\"_CLASS\":\"Player\", \"_METHOD\":\"Hello\", \"_ARGS\":[{\"allo\":1, \"allo2\":2}]}";
+        bool callback = false;
+        void Start()
+        {
+            ws.Connect();
+            ws.Send("Bonjour");
+            DateTime before = DateTime.Now;
+            ws.OnMessage += (sender, e) => {
+                Debug.Log(e.Data);
+                callback = true;
+                Debug.Log(DateTime.Now - before);
+            };
+        }
+
+        private void Update()
+        {
+            if(callback)
+                ws.Close();
+        }
+    }
+}
