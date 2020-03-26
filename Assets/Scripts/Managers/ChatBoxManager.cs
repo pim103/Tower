933b5beeedb17c6944154ee750f9ce78d10da53d
Restyled.io <commits@restyled.io>
2020-03-26 17:31:49 +0000

    Restyled by astyle

--- Assets/Scripts/Managers/ChatBoxManager.cs
+++ Assets/Scripts/Managers/ChatBoxManager.cs
@@ -79,9 +79,9 @@ public class ChatBoxManager : MonoBehaviour
 
         switch (messageType)
         {
-            case Message.MessageType.playerMessage:
-                color = playerMessage;
-                break;
+        case Message.MessageType.playerMessage:
+            color = playerMessage;
+            break;
         }
 
         return color;
