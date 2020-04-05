e9f3eefcbc380d5df87c946c6f68777dd4b5402c
Restyled.io <commits@restyled.io>
2020-04-05 02:27:28 +0000

    Restyled by clang-format

--- Assets/Scripts/Games/GameController.cs
+++ Assets/Scripts/Games/GameController.cs
@@ -7,90 +7,79 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Games {
-public class GameController : MonoBehaviour
-{
-    [SerializeField]
-    private ObjectsInScene objectsInScene;
+public class GameController : MonoBehaviour {
+  [SerializeField]
+  private ObjectsInScene objectsInScene;
 
-    [SerializeField]
-    private TransitionMenuGame transitionMenuGame;
+  [SerializeField]
+  private TransitionMenuGame transitionMenuGame;
 
-    [SerializeField]
-    private ScriptsExposer se;
-    [SerializeField]
-    private string endPoint;
-    [SerializeField]
-    private string roomId;
+  [SerializeField]
+  private ScriptsExposer se;
+  [SerializeField]
+  private string endPoint;
+  [SerializeField]
+  private string roomId;
 
-    public static string staticRoomId;
+  public static string staticRoomId;
 
-    private string canStart = null;
+  private string canStart = null;
 
-    public static int PlayerIndex;
+  public static int PlayerIndex;
 
-    private bool idAssigned = false;
+  private bool idAssigned = false;
 
-    private bool otherPlayerDie = false;
+  private bool otherPlayerDie = false;
 
-    /*
-     * Flag to skip defensePhase
-     */
-    public bool byPassDefense = true;
+  /*
+   * Flag to skip defensePhase
+   */
+  public bool byPassDefense = true;
 
-    public static string mapReceived;
+  public static string mapReceived;
 
-    private IEnumerator CheckEndInit()
-    {
-        yield return new WaitForSeconds(0.1f);
-        transitionMenuGame.WantToStartGame();
+  private IEnumerator CheckEndInit() {
+    yield return new WaitForSeconds(0.1f);
+    transitionMenuGame.WantToStartGame();
+  }
+
+  private IEnumerator WaitingDeathOtherPlayer() {
+    while (!otherPlayerDie) {
+      yield return new WaitForSeconds(1f);
     }
 
+    Cursor.lockState = CursorLockMode.None;
+    SceneManager.LoadScene("MenuScene");
+  }
 
+  // ================================== BASIC METHODS
+  // ======================================
 
-    private IEnumerator WaitingDeathOtherPlayer()
-    {
-        while (!otherPlayerDie)
-        {
-            yield return new WaitForSeconds(1f);
-        }
+  // Start is called before the first frame update
+  void Start() {
+    staticRoomId = roomId;
 
-        Cursor.lockState = CursorLockMode.None;
-        SceneManager.LoadScene("MenuScene");
-    }
+    objectsInScene.mainCamera.SetActive(true);
+    PlayerIndex = 0;
 
-    // ================================== BASIC METHODS ======================================
+    TowersWebSocket.wsGame.OnMessage += (sender, args) => {
+      if (args.Data.Contains("GRID")) {
+        mapReceived = args.Data;
+      }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        staticRoomId = roomId;
+      if (args.Data.Contains("DEATH")) {
+        Debug.Log("Vous avez gagné");
+        otherPlayerDie = true;
+      }
 
-        objectsInScene.mainCamera.SetActive(true);
-        PlayerIndex = 0;
+      if (args.Data.Contains("WON")) {
+        Debug.Log("Un autre joueur a gagné");
+        otherPlayerDie = true;
+      }
+    };
 
-        TowersWebSocket.wsGame.OnMessage += (sender, args) =>
-        {
-
-            if (args.Data.Contains("GRID"))
-            {
-                mapReceived = args.Data;
-            }
-
-            if (args.Data.Contains("DEATH"))
-            {
-                Debug.Log("Vous avez gagné");
-                otherPlayerDie = true;
-            }
-
-            if (args.Data.Contains("WON"))
-            {
-                Debug.Log("Un autre joueur a gagné");
-                otherPlayerDie = true;
-            }
-        };
-
-        StartCoroutine(WaitingDeathOtherPlayer());
-    }
-    // TODO : Control player's movement here and not in PlayerMovement
+    StartCoroutine(WaitingDeathOtherPlayer());
+  }
+  // TODO : Control player's movement here and not in PlayerMovement
 }
 }
--- Assets/Scripts/Menus/ConnectionMenu.cs
+++ Assets/Scripts/Menus/ConnectionMenu.cs
@@ -5,81 +5,65 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-namespace Menus
-{
-public class ConnectionMenu : MonoBehaviour, MenuInterface
-{
-    [SerializeField]
-    private MenuController mc;
+namespace Menus {
+public class ConnectionMenu : MonoBehaviour, MenuInterface {
+  [SerializeField]
+  private MenuController mc;
 
-    [SerializeField]
-    private InputField loginField;
+  [SerializeField]
+  private InputField loginField;
 
-    [SerializeField]
-    private InputField passwordField;
+  [SerializeField]
+  private InputField passwordField;
 
-    [SerializeField]
-    private Button connectButton;
+  [SerializeField]
+  private Button connectButton;
 
-    [SerializeField]
-    private Button createButton;
+  [SerializeField]
+  private Button createButton;
 
-    [SerializeField]
-    private Button quitButton;
-    public string[] httpResponse;
+  [SerializeField]
+  private Button quitButton;
+  public string[] httpResponse;
 
-    private void Start()
-    {
-        passwordField.inputType = InputField.InputType.Password;
-        createButton.onClick.AddListener(delegate {
-            mc.ActivateMenu(MenuController.Menu.Registration);
-        });
-        connectButton.onClick.AddListener(CallLogin);
-        quitButton.onClick.AddListener(mc.QuitGame);
+  private void Start() {
+    passwordField.inputType = InputField.InputType.Password;
+    createButton.onClick.AddListener(
+        delegate { mc.ActivateMenu(MenuController.Menu.Registration); });
+    connectButton.onClick.AddListener(CallLogin);
+    quitButton.onClick.AddListener(mc.QuitGame);
+  }
+
+  private void CallLogin() { StartCoroutine(Login()); }
+
+  IEnumerator Login() {
+    WWWForm form = new WWWForm();
+    form.AddField("accountEmail", loginField.text);
+    form.AddField("accountPassword", passwordField.text);
+    form.AddField("gameToken", NetworkingController.GameToken);
+    var www = UnityWebRequest.Post(
+        "https://towers.heolia.eu/services/account/logging.php", form);
+    www.certificateHandler = new AcceptCertificate();
+    yield return www.SendWebRequest();
+    yield return new WaitForSeconds(0.5f);
+    if (www.responseCode == 202) {
+      httpResponse = www.downloadHandler.text.Split('#');
+      NetworkingController.NickName = httpResponse[0];
+      NetworkingController.AuthToken = httpResponse[1];
+      NetworkingController.AuthRole = httpResponse[2];
+      yield return new WaitForSeconds(0.5f);
+      TowersWebSocket.InitializeWebsocketEndpoint();
+      TowersWebSocket.StartConnection();
+      mc.ActivateMenu(MenuController.Menu.MainMenu);
+    } else if (www.responseCode == 406) {
+      Debug.Log("Erreur dans les informations de connextion");
+    } else {
+      Debug.Log(www.responseCode);
+      Debug.Log(www.downloadHandler.text);
+      Debug.Log("Serveur d'authentification indisponible.");
     }
+  }
 
-    private void CallLogin()
-    {
-        StartCoroutine(Login());
-    }
-
-    IEnumerator Login()
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("accountEmail", loginField.text);
-        form.AddField("accountPassword", passwordField.text);
-        form.AddField("gameToken", NetworkingController.GameToken);
-        var www = UnityWebRequest.Post("https://towers.heolia.eu/services/account/logging.php", form);
-        www.certificateHandler = new AcceptCertificate();
-        yield return www.SendWebRequest();
-        yield return new WaitForSeconds(0.5f);
-        if (www.responseCode == 202)
-        {
-            httpResponse = www.downloadHandler.text.Split('#');
-            NetworkingController.NickName = httpResponse[0];
-            NetworkingController.AuthToken = httpResponse[1];
-            NetworkingController.AuthRole = httpResponse[2];
-            yield return new WaitForSeconds(0.5f);
-            TowersWebSocket.InitializeWebsocketEndpoint();
-            TowersWebSocket.StartConnection();
-            mc.ActivateMenu(MenuController.Menu.MainMenu);
-        }
-        else if (www.responseCode == 406)
-        {
-            Debug.Log("Erreur dans les informations de connextion");
-        }
-        else
-        {
-            Debug.Log(www.responseCode);
-            Debug.Log(www.downloadHandler.text);
-            Debug.Log("Serveur d'authentification indisponible.");
-        }
-    }
-
-
-    public void InitMenu()
-    {
-        Debug.Log("Connection Menu");
-    }
+  public void InitMenu() { Debug.Log("Connection Menu"); }
 }
 }
