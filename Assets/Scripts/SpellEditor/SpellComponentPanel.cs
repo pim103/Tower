5e32c7ba48984747c01c235e7157df61de02598e
Pim <pim103@hotmail.fr>
2020-05-17 17:04:34 +0200

    Add spell panel - add spellComponent basic panel

---
+++ Assets/Scripts/SpellEditor/SpellComponentPanel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using Games.Global;
+using Games.Global.Spells;
+using Games.Global.Spells.SpellsController;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpellEditor
+{
+    public class SpellComponentPanel : MonoBehaviour
+    {
+        [SerializeField] private InputField nameSpellComponent;
+        [SerializeField] private Button save;
+        
+        [SerializeField] private Dropdown typeSpell;
+        [SerializeField] private Dropdown typeDamage;
+        [SerializeField] private Dropdown position;
+        [SerializeField] private Dropdown direction;
+
+        [SerializeField] private Toggle isBasicAttack;
+        [SerializeField] private Toggle positionMidEntity;
+        [SerializeField] private Toggle castByPassive;
+
+        [SerializeField] private GameObject[] panelsTypeSpell;
+
+        private Dictionary<string, GameObject> panels;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            save.onClick.AddListener(SaveCurrentPanel);
+
+            panels = new Dictionary<string, GameObject>();
+            foreach (GameObject go in panelsTypeSpell)
+            {
+                panels.Add(go.name, go);
+            }
+            
+            string[] enumNames = Enum.GetNames(typeof(TypeSpell));
+            List<string> listNames = new List<string>(enumNames);
+            typeSpell.AddOptions(listNames);
+
+            enumNames = Enum.GetNames(typeof(DamageType));
+            listNames = new List<string>(enumNames);
+            typeDamage.AddOptions(listNames);
+
+            enumNames = Enum.GetNames(typeof(OriginalPosition));
+            listNames = new List<string>(enumNames);
+            position.AddOptions(listNames);
+            
+            enumNames = Enum.GetNames(typeof(OriginalDirection));
+            listNames = new List<string>(enumNames);
+            direction.AddOptions(listNames);
+            
+            typeSpell.onValueChanged.AddListener(ChangeTypeSpell);
+            ChangeTypeSpell(typeSpell.value);
+        }
+
+        private void DeactivateAllPanel()
+        {
+            foreach (GameObject go in panelsTypeSpell)
+            {
+                go.SetActive(false);
+            }
+        }
+        
+        public void ChangeTypeSpell(int newIndex)
+        {
+            TypeSpell newTypeSpell = (TypeSpell) newIndex;
+
+            DeactivateAllPanel();
+
+            switch (newTypeSpell)
+            {
+                case TypeSpell.Buff:
+                    panels["BuffPanel"].SetActive(true);
+                    break;
+                case TypeSpell.Movement:
+                    panels["MovementPanel"].SetActive(true);
+                    break;
+                case TypeSpell.Passive:
+                    panels["PassivePanel"].SetActive(true);
+                    break;
+                case TypeSpell.Projectile:
+                    panels["ProjectilePanel"].SetActive(true);
+                    break;
+                case TypeSpell.Summon:
+                    panels["SummonPanel"].SetActive(true);
+                    break;
+                case TypeSpell.Transformation:
+                    panels["TransformationPanel"].SetActive(true);
+                    break;
+                case TypeSpell.Wave:
+                    panels["WavePanel"].SetActive(true);
+                    break;
+                case TypeSpell.AreaOfEffect:
+                    panels["AreaOfEffectPanel"].SetActive(true);
+                    break;
+            }
+        }
+
+        public void SaveCurrentPanel()
+        {
+            ResetCurrentSpellComponent();
+        }
+
+        public void ResetCurrentSpellComponent()
+        {
+            typeDamage.value = 0;
+            typeSpell.value = 0;
+            position.value = 0;
+            direction.value = 0;
+
+            isBasicAttack.isOn = false;
+            positionMidEntity.isOn = false;
+            castByPassive.isOn = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SpellEditor/SpellPanel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Games.Global.Spells;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpellEditor
+{
+    public class SpellPanel : MonoBehaviour
+    {
+        [SerializeField] private Button save;
+        [SerializeField] private InputField nameSpell;
+        
+        [SerializeField] private InputField cooldown;
+        [SerializeField] private InputField cost;
+        [SerializeField] private InputField castTime;
+        [SerializeField] private InputField nbUse;
+
+        [SerializeField] private Toggle deactivePassive;
+        [SerializeField] private Toggle canCastDuringCast;
+        [SerializeField] private Toggle interruptCast;
+        [SerializeField] private Toggle canRecast;
+
+        [SerializeField] private Dropdown activeSpellComponent;
+        [SerializeField] private Dropdown passiveSpellComponent;
+        [SerializeField] private Dropdown duringSpellComponent;
+        [SerializeField] private Dropdown recastSpellComponent;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            save.onClick.AddListener(SaveCurrentPanel);
+        }
+        
+        public void SaveCurrentPanel()
+        {
+            if (nameSpell.text == "" || (activeSpellComponent.value == 0 && passiveSpellComponent.value == 0))
+            {
+                return;
+            }
+
+            if (cooldown.text == "")
+            {
+                cooldown.text = "0";
+            }
+            if (cost.text == "")
+            {
+                cost.text = "0";
+            }
+            if (castTime.text == "")
+            {
+                castTime.text = "0";
+            }
+            if (nbUse.text == "")
+            {
+                nbUse.text = "-1";
+            }
+
+            Spell spell = new Spell
+            {
+                cooldown = Int32.Parse(cooldown.text),
+                cost = Int32.Parse(cost.text),
+                castTime = Int32.Parse(castTime.text),
+                nbUse = Int32.Parse(nbUse.text),
+                canRecast = canRecast.isOn,
+                canCastDuringCast = canCastDuringCast.isOn,
+                interruptCurrentCast = interruptCast.isOn,
+                deactivatePassiveWhenActive = deactivePassive.isOn,
+                activeSpellComponent = activeSpellComponent.value != 0 ? ListCreatedElement.SpellComponents[activeSpellComponent.options[activeSpellComponent.value].text] : null,
+                passiveSpellComponent = passiveSpellComponent.value != 0 ? ListCreatedElement.SpellComponents[passiveSpellComponent.options[passiveSpellComponent.value].text] : null,
+                recastSpellComponent = recastSpellComponent.value != 0 ? ListCreatedElement.SpellComponents[recastSpellComponent.options[recastSpellComponent.value].text] : null,
+                duringCastSpellComponent = duringSpellComponent.value != 0 ? ListCreatedElement.SpellComponents[duringSpellComponent.options[duringSpellComponent.value].text] : null
+            };
+
+            ListCreatedElement.Spell.Add(nameSpell.text, spell);
+
+            ResetCurrentSpell();
+        }
+
+        public void ResetCurrentSpell()
+        {
+            cooldown.text = "";
+            cost.text = "";
+            castTime.text = "";
+            nbUse.text = "";
+            canRecast.isOn = false;
+            canCastDuringCast.isOn = false;
+            interruptCast.isOn = false;
+            deactivePassive.isOn = false;
+
+            activeSpellComponent.value = 0;
+            passiveSpellComponent.value = 0;
+            recastSpellComponent.value = 0;
+            duringSpellComponent.value = 0;
+        }
+    }
+}
