5fe107e001ab7a661a34e9d3a8c217d659870c57
Benoît <benoitremy74@gmail.com>
2020-05-29 16:48:40 +0200

    Maangers arrangement

--- Assets/Scripts/Managers/GameManagers.cs
+++ Assets/Scripts/Managers/GameManagers.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Managers : MonoBehaviour
+public class GameManagers : MonoBehaviour
 {
-    public static Managers instance;
+    public static GameManagers instance;
 
     private void Awake()
     {
         if (instance != null)
         {
             //Destroy(gameObject);
-            Destroy(Managers.instance.gameObject);
+            Destroy(GameManagers.instance.gameObject);
         }
 
         instance = this;
