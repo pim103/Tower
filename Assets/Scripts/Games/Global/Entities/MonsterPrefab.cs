7cd71d8f2b99bfaa56e49c001b8393a27935e3c8
Restyled.io <commits@restyled.io>
2020-07-11 18:10:48 +0000

    Restyled by astyle

--- Assets/Scripts/Games/Global/Entities/MonsterPrefab.cs
+++ Assets/Scripts/Games/Global/Entities/MonsterPrefab.cs
@@ -8,82 +8,82 @@ using Slider = UnityEngine.UI.Slider;
 
 namespace Games.Global.Entities
 {
-    public class MonsterPrefab : EntityPrefab
+public class MonsterPrefab : EntityPrefab
+{
+    [SerializeField] private Slider hpBar;
+
+    private PlayerPrefab playerPrefab;
+
+    private Monster monster;
+
+    private void Start()
     {
-        [SerializeField] private Slider hpBar;
-
-        private PlayerPrefab playerPrefab;
-
-        private Monster monster;
-
-        private void Start()
+        if (entity == null)
         {
-            if (entity == null)
-            {
-                monster = new Monster();
-                monster.def = 1;
-                monster.initialDef = 1;
-                
-                monster.InitMonster(this);
-                monster.InitEntityList();
-                monster.weaponOriginalName = "Basic_Bow";
-                //TODO : change to monster
-                monster.BehaviorType = BehaviorType.Player;
-                monster.AttackBehaviorType = AttackBehaviorType.AllSpellsIFirst;
-                
-                entity = monster;
-                entity.entityPrefab = this;
-                entity.typeEntity = TypeEntity.MOB;
+            monster = new Monster();
+            monster.def = 1;
+            monster.initialDef = 1;
 
-                monster.InitOriginalWeapon();
+            monster.InitMonster(this);
+            monster.InitEntityList();
+            monster.weaponOriginalName = "Basic_Bow";
+            //TODO : change to monster
+            monster.BehaviorType = BehaviorType.Player;
+            monster.AttackBehaviorType = AttackBehaviorType.AllSpellsIFirst;
 
-                DataObject.monsterInScene.Add(monster);
-            }
-
-            entity.playerInBack = new List<int>();
-        }
-
-        private void Update()
-        {
-            if (DataObject.playerInScene == null || DataObject.playerInScene.Count == 0)
-            {
-                return;
-            }
-
-            if (playerPrefab == null)
-            {
-                playerPrefab = DataObject.playerInScene[GameController.PlayerIndex];
-            }
-
-            float diff = (float) entity.hp / (float) entity.initialHp;
-
-            hpBar.value = diff;
-            hpBar.transform.LookAt(playerPrefab.camera.transform);
-            hpBar.transform.Rotate(Vector3.up * 180);
-        }
-
-        public void SetMonster(Monster monster)
-        {
             entity = monster;
             entity.entityPrefab = this;
-            this.monster = monster;
+            entity.typeEntity = TypeEntity.MOB;
+
+            monster.InitOriginalWeapon();
+
+            DataObject.monsterInScene.Add(monster);
         }
 
-        public Monster GetMonster()
+        entity.playerInBack = new List<int>();
+    }
+
+    private void Update()
+    {
+        if (DataObject.playerInScene == null || DataObject.playerInScene.Count == 0)
         {
-            return (Monster) entity;
+            return;
         }
 
-        public override void SetInvisibility()
+        if (playerPrefab == null)
         {
-            if (entity.isInvisible)
-            {
-                meshRenderer.gameObject.SetActive(false);
-            }
-            else
-            {
-                meshRenderer.gameObject.SetActive(true);
-            }
+            playerPrefab = DataObject.playerInScene[GameController.PlayerIndex];
+        }
+
+        float diff = (float) entity.hp / (float) entity.initialHp;
+
+        hpBar.value = diff;
+        hpBar.transform.LookAt(playerPrefab.camera.transform);
+        hpBar.transform.Rotate(Vector3.up * 180);
+    }
+
+    public void SetMonster(Monster monster)
+    {
+        entity = monster;
+        entity.entityPrefab = this;
+        this.monster = monster;
+    }
+
+    public Monster GetMonster()
+    {
+        return (Monster) entity;
+    }
+
+    public override void SetInvisibility()
+    {
+        if (entity.isInvisible)
+        {
+            meshRenderer.gameObject.SetActive(false);
+        }
+        else
+        {
+            meshRenderer.gameObject.SetActive(true);
         }
     }
+}
 }
--- Assets/Scripts/Games/Transitions/TransitionMenuGame.cs
+++ Assets/Scripts/Games/Transitions/TransitionMenuGame.cs
@@ -12,145 +12,145 @@ using Utils;
 
 namespace Games.Transitions
 {
-    public class TransitionMenuGame : MonoBehaviour
+public class TransitionMenuGame : MonoBehaviour
+{
+    private const int durationChooseDeckPhase = 100;
+
+    [SerializeField]
+    private ObjectsInScene objectsInScene;
+
+    [SerializeField]
+    private InitDefense initDefense;
+
+    private static int waitingForStart;
+    public static int timerAttack = Int32.MaxValue;
+
+    private string waitingGameStartText;
+
+    [SerializeField] private GameObject chooseRoleAndDeckGameObject;
+
+
+    private void Start()
     {
-        private const int durationChooseDeckPhase = 100;
-
-        [SerializeField]
-        private ObjectsInScene objectsInScene;
-
-        [SerializeField] 
-        private InitDefense initDefense;
-
-        private static int waitingForStart;
-        public static int timerAttack = Int32.MaxValue;
-
-        private string waitingGameStartText;
-
-        [SerializeField] private GameObject chooseRoleAndDeckGameObject;
-        
-        
-        private void Start()
-        {
-            waitingForStart = durationChooseDeckPhase;
-            waitingGameStartText = "Waiting for another player";
-        }
-
-        public bool InitGame()
-        {
-            CurrentRoom.loadGameDefense = false;
-            CurrentRoom.loadGameAttack = false;
-
-            StartCoroutine(LoadGame());
-            TowersWebSocket.TowerSender("SELF", NetworkingController.CurrentRoomToken,"null", "setGameLoaded", "null");
-            if (TowersWebSocket.wsGame != null)
-            {
-                TowersWebSocket.wsGame.OnMessage += (sender, args) =>
-                {
-                    if (args.Data.Contains("callbackMessages"))
-                    {
-                        fsSerializer serializer = new fsSerializer();
-                        fsData data;
-                        CallbackMessages callbackMessage = null;
-                        try
-                        {
-                            data = fsJsonParser.Parse(args.Data);
-                            serializer.TryDeserialize(data, ref callbackMessage);
-                            callbackMessage = Tools.Clone(callbackMessage);
-                            if (callbackMessage.callbackMessages.message == "LoadGame")
-                            {
-                                CurrentRoom.loadGame = true;
-                            }
-                            if (callbackMessage.callbackMessages.message == "setGameLoaded")
-                            {
-                                Debug.Log("En attente de l'adversaire");
-                            }
-
-                            if (CurrentRoom.loadGame)
-                            {
-                                if (callbackMessage.callbackMessages.roleTimer != -1)
-                                {
-                                    waitingForStart = callbackMessage.callbackMessages.roleTimer;
-                                    CurrentRoom.loadRoleAndDeck = true;
-                                }
-                                if (callbackMessage.callbackMessages.message == "StartDefense")
-                                {
-                                    CurrentRoom.loadGameDefense = true;
-                                }
-                                if (callbackMessage.callbackMessages.message == "StartAttack")
-                                {
-                                    CurrentRoom.loadGameAttack = true;
-                                }
-                                if (CurrentRoom.loadGameAttack)
-                                {
-                                    if (callbackMessage.callbackMessages.attackTimer != -1)
-                                    {
-                                        timerAttack = callbackMessage.callbackMessages.attackTimer;
-                                    }
-                                }
-                            }
-                            
-                        }
-                        catch (Exception e)
-                        {
-                            Debug.Log("Can't read callback : " + e.Message);
-                        }
-                    }
-                };
-            }
-
-            return true;
-        }
-
-        public IEnumerator LoadGame()
-        {
-            while (!CurrentRoom.loadGame)
-            {
-                yield return new WaitForSeconds(0.1f);
-            }
-            SceneManager.LoadScene("GameScene");
-        }
-
-        public IEnumerator WaitingForStart()
-        {
-            while (!CurrentRoom.loadRoleAndDeck)
-            {
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            chooseRoleAndDeckGameObject.SetActive(true);
-            
-            while (waitingForStart > 0 && !ChooseDeckAndClass.isValidate)
-            {
-                objectsInScene.waitingText.text = waitingGameStartText;
-                objectsInScene.counterText.text = waitingForStart.ToString();
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            waitingForStart = durationChooseDeckPhase;
-            TowersWebSocket.TowerSender("SELF", NetworkingController.CurrentRoomToken,"null", "setDefenseReady", "null");
-            while (!CurrentRoom.loadGameDefense)
-            {
-                yield return new WaitForSeconds(0.5f);
-            }
-            // TODO : Need RPC to launch game
-            StartGameWithDefense();
-        }
-
-        public void WantToStartGame()
-        {
-            waitingGameStartText = "La partie commence dans     secondes";
-            StartCoroutine(WaitingForStart());
-        }
-
-        public void StartGameWithDefense()
-        {
-            chooseRoleAndDeckGameObject.SetActive(false);
-            objectsInScene.mainCamera.SetActive(false);
-
-            objectsInScene.containerAttack.SetActive(false);
-            objectsInScene.containerDefense.SetActive(true);
-            initDefense.Init();
-        }
+        waitingForStart = durationChooseDeckPhase;
+        waitingGameStartText = "Waiting for another player";
     }
+
+    public bool InitGame()
+    {
+        CurrentRoom.loadGameDefense = false;
+        CurrentRoom.loadGameAttack = false;
+
+        StartCoroutine(LoadGame());
+        TowersWebSocket.TowerSender("SELF", NetworkingController.CurrentRoomToken,"null", "setGameLoaded", "null");
+        if (TowersWebSocket.wsGame != null)
+        {
+            TowersWebSocket.wsGame.OnMessage += (sender, args) =>
+            {
+                if (args.Data.Contains("callbackMessages"))
+                {
+                    fsSerializer serializer = new fsSerializer();
+                    fsData data;
+                    CallbackMessages callbackMessage = null;
+                    try
+                    {
+                        data = fsJsonParser.Parse(args.Data);
+                        serializer.TryDeserialize(data, ref callbackMessage);
+                        callbackMessage = Tools.Clone(callbackMessage);
+                        if (callbackMessage.callbackMessages.message == "LoadGame")
+                        {
+                            CurrentRoom.loadGame = true;
+                        }
+                        if (callbackMessage.callbackMessages.message == "setGameLoaded")
+                        {
+                            Debug.Log("En attente de l'adversaire");
+                        }
+
+                        if (CurrentRoom.loadGame)
+                        {
+                            if (callbackMessage.callbackMessages.roleTimer != -1)
+                            {
+                                waitingForStart = callbackMessage.callbackMessages.roleTimer;
+                                CurrentRoom.loadRoleAndDeck = true;
+                            }
+                            if (callbackMessage.callbackMessages.message == "StartDefense")
+                            {
+                                CurrentRoom.loadGameDefense = true;
+                            }
+                            if (callbackMessage.callbackMessages.message == "StartAttack")
+                            {
+                                CurrentRoom.loadGameAttack = true;
+                            }
+                            if (CurrentRoom.loadGameAttack)
+                            {
+                                if (callbackMessage.callbackMessages.attackTimer != -1)
+                                {
+                                    timerAttack = callbackMessage.callbackMessages.attackTimer;
+                                }
+                            }
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Log("Can't read callback : " + e.Message);
+                    }
+                }
+            };
+        }
+
+        return true;
+    }
+
+    public IEnumerator LoadGame()
+    {
+        while (!CurrentRoom.loadGame)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public IEnumerator WaitingForStart()
+    {
+        while (!CurrentRoom.loadRoleAndDeck)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        chooseRoleAndDeckGameObject.SetActive(true);
+
+        while (waitingForStart > 0 && !ChooseDeckAndClass.isValidate)
+        {
+            objectsInScene.waitingText.text = waitingGameStartText;
+            objectsInScene.counterText.text = waitingForStart.ToString();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        waitingForStart = durationChooseDeckPhase;
+        TowersWebSocket.TowerSender("SELF", NetworkingController.CurrentRoomToken,"null", "setDefenseReady", "null");
+        while (!CurrentRoom.loadGameDefense)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        // TODO : Need RPC to launch game
+        StartGameWithDefense();
+    }
+
+    public void WantToStartGame()
+    {
+        waitingGameStartText = "La partie commence dans     secondes";
+        StartCoroutine(WaitingForStart());
+    }
+
+    public void StartGameWithDefense()
+    {
+        chooseRoleAndDeckGameObject.SetActive(false);
+        objectsInScene.mainCamera.SetActive(false);
+
+        objectsInScene.containerAttack.SetActive(false);
+        objectsInScene.containerDefense.SetActive(true);
+        initDefense.Init();
+    }
+}
 }
