bfc51a5463c9da1cf7b0e36ba19ac80039ee038f
Attika <attika.mayou@gmail.com>
2020-09-28 22:58:15 +0200

    WIP popup

---
+++ Assets/Scripts/Utils/Popup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+//Author : Attika
+
+public class Popup : MonoBehaviour
+{
+    private static Popup _this;
+    public Button CancelButton;
+
+    private void Awake()
+    {
+        if(_this == null)
+            _this = this;
+        //CancelButton.clicked += Close();
+    }
+
+    public static void Open()
+    {
+        _this.gameObject.SetActive(true);
+    }
+    
+    public static void Close()
+    {
+        _this.gameObject.SetActive(false);
+    }
+    
+}
