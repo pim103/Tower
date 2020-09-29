1ab17f58de002a630bf4b367682da1b0c2c69b03
Attika <attika.mayou@gmail.com>
2020-09-29 10:58:43 +0200

    Add simple popup prefab

--- Assets/Scripts/Utils/Popup.cs
+++ Assets/Scripts/Utils/Popup.cs
@@ -1,29 +1,83 @@
-﻿using System;
+﻿using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 //Author : Attika
 
-public class Popup : MonoBehaviour
+namespace Utils
 {
-    private static Popup _this;
-    public Button CancelButton;
+    public class Popup : MonoBehaviour
+    {
+        private static Popup _this;
 
-    private void Awake()
-    {
-        if(_this == null)
-            _this = this;
-        //CancelButton.clicked += Close();
-    }
+        [SerializeField] private GameObject background;
+        [SerializeField] private TextMeshProUGUI title;
+        [SerializeField] private Button closeButton;
+        [SerializeField] private TextMeshProUGUI description;
+        private bool callbackAdded;
 
-    public static void Open()
-    {
-        _this.gameObject.SetActive(true);
+        //Attribution (instance of this popup) and initialization (add Close function to closeButton and set background to false)  
+        private void Awake()
+        {
+            if(_this == null)
+                _this = this;
+
+            _this.callbackAdded = false;
+            _this.closeButton.onClick.AddListener(Close);
+            background.SetActive(false);
+        }
+
+        #region customize attributes methods
+
+        //Customize title
+        public void SetTitle(string value)
+        {
+            _this.title.text = value;
+        }
+
+        //Customize description
+        public void SetDescription(string value)
+        {
+            _this.description.text = value;
+        }
+
+        //Add a callback to closeButton
+        public void AddListenerToCloseButton(UnityAction callback)
+        {
+            _this.closeButton.onClick.AddListener(callback);
+            if(!_this.callbackAdded)
+                _this.callbackAdded = true;
+        }
+        
+        #endregion
+        
+        #region activation and deactivation methods
+        
+        //Open popup
+        public static void Open()
+        {
+            _this.background.SetActive(true);
+        }
+
+        //Close popup, and if there is other(s) callback(s), clear them
+        private static void Close()
+        {
+            _this.background.SetActive(false);
+            if (_this.callbackAdded)
+            {
+                ReinitializeCallbacks();
+            }
+        }
+
+        //Clear all callbacks from closeButton and reinitialize with Close function
+        private static void ReinitializeCallbacks()
+        {
+            _this.closeButton.onClick.RemoveAllListeners();
+            _this.closeButton.onClick.AddListener(Close);
+            _this.callbackAdded = false;
+        }
+        
+        #endregion
     }
-    
-    public static void Close()
-    {
-        _this.gameObject.SetActive(false);
-    }
-    
 }
---
+++ Assets/TestPopup_TO_DO_DELETE.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Utils;
+
+public class TestPopup_TO_DO_DELETE : MonoBehaviour
+{
+    public TextMeshProUGUI helloWorld;
+
+    public void AddListenerToPopup(Popup popup)
+    {
+        popup.AddListenerToCloseButton(HelloWorld);
+    }
+    
+    public void HelloWorld()
+    {
+        helloWorld.gameObject.SetActive(true);
+        StartCoroutine(WaitAndEraseHello());
+    }
+
+    private IEnumerator WaitAndEraseHello()
+    {
+        yield return new WaitForSeconds(2.0f);
+        helloWorld.gameObject.SetActive(false);
+    }
+}
