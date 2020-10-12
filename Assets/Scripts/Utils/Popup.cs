3e78f19719684377fee1521ea10540af4e5654bf
Attika <attika.mayou@gmail.com>
2020-10-12 09:03:54 +0200

    WIP on options

--- Assets/Scripts/Utils/Popup.cs
+++ Assets/Scripts/Utils/Popup.cs
@@ -1,6 +1,7 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 //Author : Attika
@@ -8,6 +9,7 @@ using UnityEngine.UI;
 namespace Utils
 {
     //TODO : une fois fini de dev le systeme de popup, supprimer les commentaires dans PopupOptions (ou les réduire au strict minimum)
+    [System.Serializable]
     public class PopupOptions
     {
         public bool isContextualDialog; // La popup est prioritaire sur tous les autres ui, et on bloque les input qui ne la concerne pas tant qu'elle n'est pas fermée (en effectuant une certaine action => oui/non, fermer la popup... etc)
@@ -22,13 +24,33 @@ namespace Utils
     {
         private static Popup _this;
 
+        [Header("General Parameters")]
         [SerializeField] private GameObject background;
         [SerializeField] private TextMeshProUGUI title;
         [SerializeField] private Button closeButton;
         [SerializeField] private TextMeshProUGUI description;
         private bool callbackAdded;
-        private PopupOptions options;
+        [SerializeField] private PopupOptions options;
+        
+        [Header("Contextual Parameters")]
+        [SerializeField] private GameObject contextualUiBlock;
+        
+        [Header("Question Parameters")]
+        [SerializeField] private GameObject question;
+        [SerializeField] private TextMeshProUGUI titleQuestion;
+        [SerializeField] private TextMeshProUGUI validateButtonText;
+        [SerializeField] private Button validateButton;
+        private bool validateCallbackAdded;
+        [SerializeField] private TextMeshProUGUI cancelButtonText;
+        [SerializeField] private Button cancelButton;
+        private bool cancelCallbackAdded;
+        [SerializeField] private TextMeshProUGUI thirdButtonText;
+        [SerializeField] private Button thirdButton;
+        private bool thirdCallbackAdded;
 
+        [Header("Animation parameters")]
+        [SerializeField] private Animator animator;
+        
         //Attribution (instance of this popup) and initialization (add Close function to closeButton and set background to false)  
         private void Awake()
         {
@@ -42,6 +64,8 @@ namespace Utils
 
         #region customize attributes methods
 
+        
+        /* General customization */
         //Customize title
         public void SetTitle(string value)
         {
@@ -60,6 +84,71 @@ namespace Utils
             _this.closeButton.onClick.AddListener(callback);
             if(!_this.callbackAdded)
                 _this.callbackAdded = true;
+        }
+
+        /* Question customization */
+        //Customize question title
+        public void SetTitleQuestion(string value)
+        {
+            if (options.isAQuestion)
+                Debug.LogWarning(
+                    "Trying to customize popup's question attributes but the question option is not checked on this popup");
+            else
+                titleQuestion.text = value;
+        }
+
+        //Customize validate text
+        public void SetValidateButtonText(string value)
+        {
+            if (options.isAQuestion)
+                Debug.LogWarning(
+                    "Trying to customize popup's question attributes but the question option is not checked on this popup");
+            else
+                validateButtonText.text = value;
+        }
+        
+        //Customize cancel text
+        public void SetCancelButtonText(string value)
+        {
+            if (options.isAQuestion)
+                Debug.LogWarning(
+                    "Trying to customize popup's question attributes but the question option is not checked on this popup");
+            else
+                cancelButtonText.text = value;
+        }
+        
+        //Customize third button text
+        public void SetThirdButtonText(string value)
+        {
+            if (options.isAQuestion)
+                Debug.LogWarning(
+                    "Trying to customize popup's question attributes but the question option is not checked on this popup");
+            else
+                thirdButtonText.text = value;
+        }
+        
+        //Add a callback on validate button
+        public void AddListenerToValidateButton(UnityAction callback)
+        {
+            _this.validateButton.onClick.AddListener(callback);
+            if(!_this.validateCallbackAdded)
+                _this.validateCallbackAdded = true;
+        }
+        
+        //Add a callback on cancel button
+        public void AddListenerToCancelButton(UnityAction callback)
+        {
+            _this.cancelButton.onClick.AddListener(callback);
+            if(!_this.cancelCallbackAdded)
+                _this.cancelCallbackAdded = true;
+        }
+        
+        //Add a callback on third button
+        public void AddListenerToThirdButton(UnityAction callback)
+        {
+            _this.thirdButton.onClick.AddListener(callback);
+            if (!_this.thirdCallbackAdded)
+                _this.thirdCallbackAdded = true;
         }
         
         #endregion
