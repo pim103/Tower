5c3f8309d882ae02fd95b5ebeb690de998969372
Attika <attika.mayou@gmail.com>
2020-10-27 12:43:55 +0100

    Customize Editor for popup options

--- Assets/Scripts/Utils/Popup.cs
+++ Assets/Scripts/Utils/Popup.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -7,35 +8,33 @@ using UnityEngine.UI;
 
 namespace Utils
 {
-    //TODO : une fois fini de dev le système de popup, supprimer les commentaires dans PopupOptions (ou les réduire au strict minimum)
-    [System.Serializable]
-    public class PopupOptions
-    {
-        public bool isContextualDialog; // La popup est prioritaire sur tous les autres ui, et on bloque les input qui ne la concerne pas tant qu'elle n'est pas fermée (en effectuant une certaine action => oui/non, fermer la popup... etc)
-        public bool isDynamicPosition; // La popup doit se placer dynamiquement à l'écran par rapport à une autre ui (droite/gauche, haut/bas) selon la place dispo
-        public bool isDynamicSize; // La popup doit être resize selon le contenu (qui n'est pas déterminé à l'avance) => toutes les popup ne devraient-elles pas avoir cette règle ? 
-        public bool isAQuestion; // La popup doit contenir deux boutons (voire trois) à laquelle une action doit être liée 
-        public bool isAnimated; // La popup a une petite animation d'apparition (et de disparition ?)
-        //TODO : d'autres choses à ajouter ?    
-    }
-
     public class Popup : MonoBehaviour
     {
         private static Popup _this;
 
         [Header("General Parameters")]
         [SerializeField] private GameObject content;
+        [SerializeField] private Image background;
         [SerializeField] private TextMeshProUGUI title;
         [SerializeField] private Button closeButton;
         [SerializeField] private TextMeshProUGUI description;
         private bool callbackAdded;
-        private PopupOptions options;
-
-        [Header("Contextual Parameters")] public bool isContextualDialog;
+        
+        #region options parameters
+        
+        [Header("Contextual Parameters")] public bool isContextualDialog; //La popup est prioritaire sur tous les autres ui, et on bloque les input qui ne la concerne pas tant qu'elle n'est pas fermée (en effectuant une certaine action => oui/non, fermer la popup... etc)
         [DrawIf("isContextualDialog", true, ComparisonType.Equals, DisablingType.DontDraw)]
         [SerializeField] private GameObject contextualUiBlock;
 
-        [Header("Question Parameters")] public bool isAQuestion;
+        [Header("Dynamic Parameters")] public bool isDynamic; 
+        [DrawIf("isDynamic", true, ComparisonType.Equals, DisablingType.DontDraw)] 
+        [SerializeField] private bool isPositionDynamic; //La popup doit se placer dynamiquement à l'écran par rapport à une autre ui (droite/gauche, haut/bas) selon la place dispo
+        [DrawIf("isPositionDynamic", true, ComparisonType.Equals, DisablingType.DontDraw)] 
+        [SerializeField] private GameObject otherObj;
+        [DrawIf("isDynamic", true, ComparisonType.Equals, DisablingType.DontDraw)] 
+        [SerializeField] private bool isSizeDynamic; //La popup doit être resize selon le contenu (qui n'est pas déterminé à l'avance) => toutes les popup ne devraient-elles pas avoir cette règle ? 
+
+        [Header("Question Parameters")] public bool isAQuestion; //La popup doit contenir deux boutons (voire trois) à laquelle une action doit être liée
         [DrawIf("isAQuestion", true, ComparisonType.Equals, DisablingType.DontDraw)]
         [SerializeField] private GameObject question;
         [DrawIf("isAQuestion", true, ComparisonType.Equals, DisablingType.DontDraw)]
@@ -58,9 +57,11 @@ namespace Utils
         [SerializeField] private Button thirdButton;
         private bool thirdCallbackAdded;
 
-        [Header("Animation parameters")] public bool isAnimated;
+        [Header("Animation parameters")] public bool isAnimated; //La popup a une petite animation d'apparition (et de disparition ?)
         [DrawIf("isAnimated", true, ComparisonType.Equals, DisablingType.DontDraw)]
         [SerializeField] private Animator animator;
+        
+        #endregion
         
         //Attribution (instance of this popup) and initialization (add Close function to closeButton and set background to false)  
         private void Awake()
@@ -75,10 +76,20 @@ namespace Utils
             InitializeOptionsAttributes();
         }
 
-        #region customize attributes methods
+        private void OnEnable()
+        {
+            InitializeOptionsAttributes();
+        }
 
+        #region customize attributes methods
         
         /* General customization */
+        //Customize image background
+        public void SetBackground(Sprite image)
+        {
+            _this.background.sprite = image;
+        }
+        
         //Customize title
         public void SetTitle(string value)
         {
@@ -103,7 +114,7 @@ namespace Utils
         //Customize question title
         public void SetTitleQuestion(string value)
         {
-            if (options.isAQuestion)
+            if (!isAQuestion)
                 Debug.LogWarning(
                     "Trying to customize popup's question attributes but the question option is not checked on this popup");
             else
@@ -113,7 +124,7 @@ namespace Utils
         //Customize validate text
         public void SetValidateButtonText(string value)
         {
-            if (options.isAQuestion)
+            if (!isAQuestion)
                 Debug.LogWarning(
                     "Trying to customize popup's question attributes but the question option is not checked on this popup");
             else
@@ -123,7 +134,7 @@ namespace Utils
         //Customize cancel text
         public void SetCancelButtonText(string value)
         {
-            if (options.isAQuestion)
+            if (!isAQuestion)
                 Debug.LogWarning(
                     "Trying to customize popup's question attributes but the question option is not checked on this popup");
             else
@@ -133,7 +144,7 @@ namespace Utils
         //Customize third button text
         public void SetThirdButtonText(string value)
         {
-            if (options.isAQuestion)
+            if (!isAQuestion)
                 Debug.LogWarning(
                     "Trying to customize popup's question attributes but the question option is not checked on this popup");
             else
@@ -143,25 +154,54 @@ namespace Utils
         //Add a callback on validate button
         public void AddListenerToValidateButton(UnityAction callback)
         {
-            _this.validateButton.onClick.AddListener(callback);
-            if(!_this.validateCallbackAdded)
-                _this.validateCallbackAdded = true;
+            if (!isAQuestion)
+                Debug.LogWarning(
+                    "Trying to customize popup's question attributes but the question option is not checked on this popup");
+            else
+            {
+                _this.validateButton.onClick.AddListener(callback);
+                if(!_this.validateCallbackAdded)
+                    _this.validateCallbackAdded = true;
+            }
         }
         
         //Add a callback on cancel button
         public void AddListenerToCancelButton(UnityAction callback)
-        {
-            _this.cancelButton.onClick.AddListener(callback);
-            if(!_this.cancelCallbackAdded)
-                _this.cancelCallbackAdded = true;
+        { if (!isAQuestion)
+                Debug.LogWarning(
+                    "Trying to customize popup's question attributes but the question option is not checked on this popup");
+            else
+            {
+                _this.cancelButton.onClick.AddListener(callback);
+                if (!_this.cancelCallbackAdded)
+                    _this.cancelCallbackAdded = true;
+            }
         }
         
         //Add a callback on third button
         public void AddListenerToThirdButton(UnityAction callback)
+        { 
+            if (!isAQuestion)
+                Debug.LogWarning(
+                    "Trying to customize popup's question attributes but the question option is not checked on this popup");
+            else
+            {
+                _this.thirdButton.onClick.AddListener(callback);
+                if (!_this.thirdCallbackAdded)
+                    _this.thirdCallbackAdded = true;
+            }
+        }
+        
+        /* Dynamic customization */
+        //Set the other object the popup should be dynamic to
+        //TODO : should there be several objects the popup can be dynamically positioned next to ?
+        public void AddDynamicObject(GameObject other)
         {
-            _this.thirdButton.onClick.AddListener(callback);
-            if (!_this.thirdCallbackAdded)
-                _this.thirdCallbackAdded = true;
+            if (!_this.isPositionDynamic)
+                Debug.LogWarning(
+                    "Trying to customize popup's dynamic position attributes but the option is not checked on this popup");
+            else
+                _this.otherObj = other;
         }
         
         #endregion
@@ -178,7 +218,7 @@ namespace Utils
         private static void Close()
         {
             _this.content.SetActive(false);
-            if (_this.callbackAdded)
+            if (_this.callbackAdded || _this.isAQuestion && (_this.validateCallbackAdded || _this.cancelCallbackAdded || _this.thirdCallbackAdded))
             {
                 ReinitializeCallbacks();
             }
@@ -190,36 +230,48 @@ namespace Utils
             _this.closeButton.onClick.RemoveAllListeners();
             _this.closeButton.onClick.AddListener(Close);
             _this.callbackAdded = false;
+            if (_this.isAQuestion)
+            {
+                _this.validateButton.onClick.RemoveAllListeners();
+                _this.cancelButton.onClick.RemoveAllListeners();
+                _this.thirdButton.onClick.RemoveAllListeners();
+                _this.validateCallbackAdded = false;
+                _this.cancelCallbackAdded = false;
+                _this.thirdCallbackAdded = false;
+            }
         }
         
         #endregion
         
         #region options methods
 
-        private void InitializeOptionsAttributes()
+        //
+        private void InitializeOptionsAttributes() // Traitements à l'initialisation selon les options choisies
         {
-            if (_this.options.isContextualDialog)
-            {
+            if (contextualUiBlock != null)
+                contextualUiBlock.SetActive(isContextualDialog);
                 
-            }
-            if (_this.options.isDynamicPosition)
+            if (question != null)
             {
-                
-            }
-            if (_this.options.isDynamicSize)
-            {
-                
-            }
-            if (_this.options.isAQuestion)
-            {
+                question.SetActive(isAQuestion);
                 //Est-ce qu'on force le contextual dialog en cas de question ? 
-            }
-            if (_this.options.isAnimated)
-            {
-                
             }
         }
         
+        #endregion
+        
+        #region edition methods
+
+        //Allow editor updates
+        private void OnValidate()
+        {
+            if (contextualUiBlock != null)
+                contextualUiBlock.SetActive(isContextualDialog);
+            
+            if(question != null)
+                question.SetActive(isAQuestion);
+        }
+
         #endregion
     }
 }
